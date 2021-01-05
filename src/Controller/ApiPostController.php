249eda050ba1d50c91e9f66c428e9edf64c59f31
jordan62300 <jordanfievetpro@outlook.fr>
2021-01-05 12:57:29 +0100

    ajout du catch d'exeption pour l'api

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -9,7 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ApiPostController extends AbstractController
 {
@@ -31,19 +33,32 @@ class ApiPostController extends AbstractController
     /**
      * @Route("/api/post", name="api_post_create", methods={"POST"})
      */
-    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $em)
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $em ,ValidatorInterface $validator)
     {
         $json = $request->getContent();
 
+        try{
         $post = $serializer->deserialize($json,Post::class,'json');
 
         $post->setCreatedAt(new \DateTime());
+
+        $errors = $validator->validate($post);
+
+        if(count($errors) > 0) {
+            return $this->json($errors,400);
+        }
 
         $em->persist($post);
         $em->flush();
 
         return $this->json($post,201,[],["groups" => "post:read"]);
-
+        }
+        catch(NotEncodableValueException $e){
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ],400);
+        }
 
     }
 }
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
@@ -23,12 +24,14 @@ class Post
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups("post:read")
+     * @Assert\NotBlank
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
      * @Groups("post:read")
+     * @Assert\NotBlank
      */
     private $content;
 
