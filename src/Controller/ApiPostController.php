1af4aac5ad5ad1b0188d4f77a3dd3402e0fc9699
jordan62300 <jordanfievetpro@outlook.fr>
2021-01-05 11:50:13 +0100

    ajout de l'api pour tous les posts

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiPostController extends AbstractController
 {
     /**
-     * @Route("/api/post", name="api_post")
+     * @Route("/api/post", name="api_post_index", methods={"GET"})
      */
-    public function index()
+    public function index(PostRepository $postRepository)
     {
-        return $this->render('api_post/index.html.twig', [
-            'controller_name' => 'ApiPostController',
-        ]);
+
+      //  $posts = $postRepository->findAll();
+
+   
+
+    //    $json = $serializer->serialize($posts,'json',['groups' => 'post:read']);
+        
+
+     //   $response = new JsonResponse($json,200,[],true);
+
+      return   $this->json($postRepository->findAll(),200,[],["groups" => 'post:read']);
+
+        
+
+    
     }
 }
